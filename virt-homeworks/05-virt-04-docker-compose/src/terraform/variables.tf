f7357d7d3d5edaa1875f8d34e3a1718532f74969
Маковецкий Ярослав <Makovetskiy@vector-best.ru>
2022-11-23 20:05:23 +0700

    05-virt-04-docker-compose

---
+++ virt-homeworks/05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gqmf4dahjg162ak8fu"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gre57qvs7kq13m0e8p"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8jo9842mmqvsvlvlrp"
+}
